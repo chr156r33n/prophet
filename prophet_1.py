f262f05e54a6013e966a23d58f51fd22a8905206
Chris Green <75560394+chr156r33n@users.noreply.github.com>
2025-05-06 16:19:02 +0100

    Update prophet_1.py

--- prophet_1.py
+++ prophet_1.py
@@ -12,9 +12,7 @@ st.set_page_config(page_title="Prophet Forecasting App", layout="wide")
 
 # Title
 st.title("Prophet Forecasting App")
-st.markdown("""
-This App takes Meta's Prophet Forecasting model and lets you apply it to SEO data to establish forecasts based on historic data. 
-""")
+st.text("This App takes Meta's Prophet Forecasting model and lets you apply it to SEO data to establish forecasts based on historic data. ")
 
 with st.expander("How to use this app"):
     st.markdown("""
